9c9c56024bae0cb75e05290c0cac052dab3e898e
BotMaster135 <amirbhaiyt1@gmail.com>
2025-03-20 23:57:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,13 +1,13 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "26597768"))
+API_HASH = environ.get("API_HASH", "340f87444e648a712eae77c310648e21")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7736867939:AAGTuiBdoyM1zfs9alpLI51p5MiBhRUDqeA")
 FILE_CAPTION = environ.get("FILE_CAPTION", "<code>{file_name}</code>")
-OWNER = environ.get("OWNER", "")
+OWNER = environ.get("OWNER", "@botmaster55")
 PRIVATE_BOT = environ.get("PRIVATE_BOT", False)
-UPDATES_CHANNEL = environ.get("UPDATES_CHANNEL", "https://t.me/webverseupdates")
-SUPPORT_GROUP = environ.get("SUPPORT_GROUP", "https://t.me/webversegroup")
-DEV_LINK = environ.get("DEV_LINK", "https://t.me/bhaiyajixbot")
+UPDATES_CHANNEL = environ.get("UPDATES_CHANNEL", "https://t.me/+iXwNgNXtOkk1OTVl")
+SUPPORT_GROUP = environ.get("SUPPORT_GROUP", "https://t.me/+aAmjR1AgZmRlOGJl")
+DEV_LINK = environ.get("DEV_LINK", "https://t.me/botmaster55")
 
 # customise as per your need
