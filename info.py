fc8abf3d4716c7a9389af2209a60932cc47fbc53
owajbhai <owajshaikhrcm@gmail.com>
2025-04-22 12:53:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "23621595"))
 API_HASH = environ.get("API_HASH", "de904be2b4cd4efe2ea728ded17ca77d")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7736867939:AAGTuiBdoyM1zfs9alpLI51p5MiBhRUDqeA")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7688007369:AAFYeEbvgegmwiKElzz-Y83QtuwBqubXFA0")
 FILE_CAPTION = environ.get("FILE_CAPTION", "<code>{file_name}</code>")
 OWNER = environ.get("OWNER", "@Ritikmoves2")
 PRIVATE_BOT = environ.get("PRIVATE_BOT", False)
