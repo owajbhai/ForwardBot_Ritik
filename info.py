1bc16220101e50db9c5c9f4e3282aeaba7692694
owajbhai <owajshaikhrcm@gmail.com>
2025-04-22 12:50:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,13 +1,13 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", "26597768"))
-API_HASH = environ.get("API_HASH", "340f87444e648a712eae77c310648e21")
+API_ID = int(environ.get("API_ID", "23621595"))
+API_HASH = environ.get("API_HASH", "de904be2b4cd4efe2ea728ded17ca77d")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7736867939:AAGTuiBdoyM1zfs9alpLI51p5MiBhRUDqeA")
 FILE_CAPTION = environ.get("FILE_CAPTION", "<code>{file_name}</code>")
-OWNER = environ.get("OWNER", "@botmaster55")
+OWNER = environ.get("OWNER", "@Ritikmoves2")
 PRIVATE_BOT = environ.get("PRIVATE_BOT", False)
-UPDATES_CHANNEL = environ.get("UPDATES_CHANNEL", "https://t.me/+iXwNgNXtOkk1OTVl")
-SUPPORT_GROUP = environ.get("SUPPORT_GROUP", "https://t.me/+aAmjR1AgZmRlOGJl")
-DEV_LINK = environ.get("DEV_LINK", "https://t.me/botmaster55")
+UPDATES_CHANNEL = environ.get("UPDATES_CHANNEL", "https://t.me/+qFspMh2clnJkNGI1")
+SUPPORT_GROUP = environ.get("SUPPORT_GROUP", "https://t.me/+qFspMh2clnJkNGI1")
+DEV_LINK = environ.get("DEV_LINK", "https://t.me/Ritikmoves2")
 
 # customise as per your need
